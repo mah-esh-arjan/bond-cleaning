c27a94c9f03363404222672efc3df2c38442ac3b
mah-esh-arjan <mah.dot98@gmail.com>
2023-10-17 14:29:50 +0545

    scratch project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavigationBar from './Navbar';
 import First from './First';
+import Second from './Second';
 function App() {
   return (
     <div>
       <NavigationBar />
       <First />
+      <Second />
     </div>
   );
 }
--- src/First.js
+++ src/First.js
@@ -4,30 +4,32 @@ import './CSS/navbar.css';
 
 function First() {
     return (
-        <div>
-            <div className="image-container">
-                <img src={service} alt="service" />
-                <div class="text-overlay">
-                    <h1 class="overlay-heading">Experience the Magic of Immaculate Spaces with Our Bond Cleaning!</h1>
-                    <p class="overlay-paragraph">Your Paragraph Here</p>
-                </div>
-                <div class="form-container">
+        <div className="image-container">
+            <img src={service} alt="service" />
+            <div class="text-overlay">
+                <h1 class="overlay-heading">Experience the Magic of Immaculate Spaces with Our Bond Cleaning!</h1>
+                <p class="overlay-paragraph">EasyBondClean is your dedicated partner for comprehensive bond cleaning services.
+                    We specialize in delivering top-quality, meticulous cleaning solutions that guarantee your bond refund, making your move-out process stress-free.
+                    With our experienced team and commitment to excellence, we ensure a seamless transition to your new place, leaving it spotless and ready for the next chapter in your life.</p>
+
+            </div>
+            <div class="form-group">
+                <div class="form-inside">
                     <form>
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" id="name" name="name" />
+                        <div>
+                            <input type="text" id="name" name="name" placeholder='Enter your FullName' />
                         </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" id="email" name="email" />
+                        <div>
+                            <input type="email" id="email" name="email" placeholder='Enter your Email' />
                         </div>
-                        <div class="form-group">
-                            <label for="phone">Phone</label>
-                            <input type="tel" id="phone" name="phone" />
+                        <div>
+                            <input type="tel" id="phone" name="phone" placeholder='Enter your Phone number' />
                         </div>
-                        <div class="form-group">
-                            <label for="address">Address</label>
-                            <input type="text" id="address" name="address" />
+                        <div>
+                            <input type="text" id="address" name="address" placeholder='Enter your address' />
+                        </div>
+                        <div>
+                            <textarea id="message" placeholder='Enter Message' name="message" rows="4" cols="50"></textarea>
                         </div>
                         <button type="submit">Submit</button>
                     </form>
