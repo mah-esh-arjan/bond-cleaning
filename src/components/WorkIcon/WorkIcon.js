dde409ab49c27fcebd48f30fb409f55b528e9d9e
raimoulik@gmail.com <raimoulik@gmail.com>
2023-10-26 22:53:04 +0545

    chore: add workIcon component

---
+++ src/components/WorkIcon/WorkIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './WorkIcon.scss'
+
+const WorkIcon = ({ children }) => {
+  return (
+    <div className='work-icon'>
+      {children}
+    </div>
+  )
+}
+
+export default WorkIcon
